6c7e3aef3eacff24bfd459c6070514e795efd88c
Nikhil Patare <31315397+nikhil-patare007@users.noreply.github.com>
2019-06-18 19:20:41 +0530

    Add files via upload
    final

---
+++ src/BinarySearchRecursive.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int binarySearch(int arr[], int l, int r, int x) 
+{ 
+   if (r >= l) 
+   { 
+        int m = l + (r - l)/2; 
+  
+        if (arr[m] == x)  return m; 
+  
+        
+        if (arr[m] > x) return binarySearch(arr, l, m-1, x); 
+  
+    
+        return binarySearch(arr, m+1, r, x); 
+   } 
+  
+    
+   return -1; 
+} 
+  
+
+int main(void) 
+{ 
+   int arr[] = {2, 3, 4, 10, 40}; 
+   int n = sizeof(arr)/ sizeof(arr[0]); 
+   int x = 3; 
+   int result = binarySearch(arr, 0, n-1, x); 
+   (result == -1)? printf("Element is not present in array") 
+                 : printf("Element is present at index %d", result); 
+   return 0; 
+} 
